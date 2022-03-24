913730288273deb6c2fa363d9c7d6c86d2f89a86
Manuel <manuelalejandromartinezf@gmail.com>
2022-03-24 07:51:32 -0600

    Archivos

---
+++ HDT7/src/Archivos.java
@@ -0,0 +1,53 @@
+import java.util.HashMap;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.BufferedReader;
+
+public class Archivos {
+    
+
+    public static HashMap<String, BinarySearchTree<Association<String, Data>>> leer(String path){
+        BufferedReader reader = new BufferedReader(new FileReader(data));
+        HashMap<String, BinarySearchTree<Association<String, Data>>> map = new HashMap<>();
+        
+        String row = reader.readLine();
+        String[] arreglo = row.split(",");
+        Data data = new Data(arreglo[1], arreglo[0], arreglo[2]);
+        map.put("spanish", new BinarySearchTree<Association<String, Data>>(new Association(arreglo[1], data)));
+        map.put("english", new BinarySearchTree<Association<String, Data>>(new Association(arreglo[0], data)));
+        map.put("french", new BinarySearchTree<Association<String, Data>>(new Association(arreglo[2], data)));
+
+
+        while ((row = reader.readLine()) != null){
+        	arreglo = row.split(",");
+            data = new Data(arreglo[1], arreglo[0], arreglo[2]);
+
+            map.get("spanish").add(new Association(arreglo[1], data));
+            map.get("english").add(new Association(arreglo[0], data));
+            map.get("french").add(new Association(arreglo[2], data));
+        }
+        reader.close();
+
+        return map;
+    }
+}
+
+
+
+
+public class Archivos {
+	
+	/**
+	 * Lee y evalua expresiones en un archivo
+	 * @param data nombre de archivo
+	 * @throws IOException
+	 */
+	public static void leer(String data) throws IOException {		
+		BufferedReader reader = new BufferedReader(new FileReader(data));
+        String row;
+        while ((row = reader.readLine()) != null){
+        	Procesador.evaluate(row);
+        }
+        reader.close();
+	}
+}
