1d5c14c192b328d9bb0791a878088083c351ec24
Manuel <manuelalejandromartinezf@gmail.com>
2022-03-24 07:42:41 -0600

    Association

--- HDT7/src/Association.java
+++ HDT7/src/Association.java
@@ -1,3 +1,29 @@
-public class Association {
-    
+public class Association<K extends Comparable<K>, V> implements Comparable<Association<K,V>> {
+
+	
+	K key;
+	V value;
+	
+	Association(K key_, V value_){
+		key = key_;
+		value = value_;
+	}
+	
+	public K getKey() {
+		return key;
+	}
+	
+	public V getValue() {
+		return value;
+	}
+	
+	
+	@Override
+	public int compareTo(Association<K, V> o) {
+		// TODO Auto-generated method stub
+		return key.compareTo(o.getKey());
+	}
+
+
+
 }
--- HDT7/src/Data.java
+++ HDT7/src/Data.java
@@ -1,4 +1,3 @@
-
 public class Data {
 	
 	String spanish;
